bdf21f7c8c48207609d360e69041b1894d5d2d82
Tania Kolesnik <kolesnik@gmail.com>
2024-12-18 15:25:55 +0000

    make build

---
+++ index.8050621b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),f=t.offsetHeight/2-e.offsetHeight/2-10,o=t.offsetWidth/2-e.offsetWidth/2-10;Object.assign(e.style,{top:f+"px",left:o+"px"});
+//# sourceMappingURL=index.8050621b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const topPos = wall.offsetHeight / 2 - spider.offsetHeight / 2 - 10;
+const leftPos = wall.offsetWidth / 2 - spider.offsetWidth / 2 - 10;
+Object.assign(spider.style, {
+    top: topPos + "px",
+    left: leftPos + "px"
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
